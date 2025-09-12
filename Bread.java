a75804ecb5df2219c9602f212bcaeea43cca20b7
MishanRana1 <mishanrana12@gmail.com>
2025-09-12 18:05:12 +0000

    Instance veriables created

--- Bread.java
+++ Bread.java
@@ -1,4 +1,14 @@
 public class Bread {
 
+    private double flour;
+    private double water;
+    private double salt;
+    private double suger;
+    private double bakingPowder;
+    private double yeast;
+    private String breadName;
+    private String state;
+    private String recipe;
+
     
 }
