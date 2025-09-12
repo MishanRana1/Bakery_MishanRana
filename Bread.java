33f82a9073246828493ad3f2908b4f290b980fc0
MishanRana1 <mishanrana12@gmail.com>
2025-09-12 18:09:19 +0000

    Default constructor made

--- Bread.java
+++ Bread.java
@@ -10,5 +10,17 @@ public class Bread {
     private String state;
     private String recipe;
 
+    public Bread() {
+        this.flour = 3.0;
+        this.water = 1.5;
+        this.salt = 1.0;
+        this.sugar = 1.0;
+        this.bakingPowder = 0.0;
+        this.yeast = 0.5;
+        this.breadName = "Generic Bread";
+        this.state = "not baked";
+        this.recipe = "1. Mix ingredients.\n2. Knead.\n3. Let rise.\n4. Bake.";
+    }
+
     
 }
